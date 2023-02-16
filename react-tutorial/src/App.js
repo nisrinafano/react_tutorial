7b9146bed1a7d38e781277333bc96bfa7cd20ed2
nisrinafano <19148635+nisrinafano@users.noreply.github.com>
2023-02-16 10:48:28 +0700

    06_adding_style
    - update css files
    - add style in jsx

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,24 +1,14 @@
 //import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const title = "Welcome";
-  const likes = 50;
-  const link = "www.google.com";
-
   return (
     <div className="App">
       <Navbar />
       <div className="content">
         <Home />
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{10+10}</p>
-        <p>{ [1,2,3,4,5] }</p>
-
-        <a href={link}>Google Site</a>
       </div>
     </div>
   );
