372970f9f0d44d53b6d2bc52572a973a21f58da5
nisrinafano <19148635+nisrinafano@users.noreply.github.com>
2023-02-20 18:24:37 +0700

    17_fetch_data_useeffect
    - fetch data inside useeffect
    - add conditional, only show data if it's already loaded

--- react-tutorial/src/Home.js
+++ react-tutorial/src/Home.js
@@ -2,29 +2,21 @@ import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: 'My blog title 1', body: 'lorem ipsum...', author: 'author1', id: 1},
-        {title: 'My blog title 2', body: 'lorem ipsum...', author: 'author2', id: 2},
-        {title: 'My blog title 3', body: 'lorem ipsum...', author: 'author3', id: 3}
-    ]);
-
-    const[name, setName] = useState('author1');
- 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
 
     useEffect(() => {
-        console.log('use effect ran');
-        console.log(name);
-    }, [name]);
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+            })
+    }, []);
 
     return ( 
         <div className="home">
-            <BlogList blogs = { blogs } title = "All blog" handleDelete = {handleDelete}/>
-            <button onClick={() => setName('author2')}>change name</button>
-            <p>{name}</p>
+            {blogs && <BlogList blogs = { blogs } title = "All blog"/>}
         </div>
      );
 }
