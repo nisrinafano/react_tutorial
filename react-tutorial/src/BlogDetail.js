d5a82c9bee7a84f37e1ccc73783c2c6f7af81ab6
nisrinafano <19148635+nisrinafano@users.noreply.github.com>
2023-02-24 07:10:53 +0700

    25_route_parameters
    - Add BlogDetail
    - Add new route with parameter
    - Add link with parameter
    - Fix link css

---
+++ react-tutorial/src/BlogDetail.js
@@ -0,0 +1,12 @@
+import { useParams } from "react-router-dom";
+
+const BlogDetail = () => {
+    const {id} = useParams()
+    return ( 
+        <div className="blog-detail">
+            <h2>Blog Details - {id}</h2>
+        </div>
+    );
+}
+ 
+export default BlogDetail;
